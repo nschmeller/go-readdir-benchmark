afcdefae6ca03128a32f15968cfa2ca843cf8893
Nick Schmeller <nick.schmeller@lacework.net>
2023-05-11 20:16:09 +0000

    Switch order of tests to check for caching
    Signed-off-by: Nick Schmeller <nick.schmeller@lacework.net>

--- main_test.go
+++ main_test.go
@@ -5,21 +5,6 @@ import (
 	"testing"
 )
 
-func BenchmarkReaddir(b *testing.B) {
-	for i := 0; i < b.N; i++ {
-		// Open a new file handle for every loop iteration to avoid caching
-		f, err := os.Open("/proc")
-		if err != nil {
-			b.Error(err)
-		}
-
-		_, err = f.Readdir(0)
-		if err != nil {
-			b.Error(err)
-		}
-	}
-}
-
 func BenchmarkReadDir(b *testing.B) {
 	for i := 0; i < b.N; i++ {
 		// Open a new file handle for every loop iteration to avoid caching
@@ -29,6 +14,21 @@ func BenchmarkReadDir(b *testing.B) {
 		}
 
 		_, err = f.ReadDir(0)
+		if err != nil {
+			b.Error(err)
+		}
+	}
+}
+
+func BenchmarkReaddir(b *testing.B) {
+	for i := 0; i < b.N; i++ {
+		// Open a new file handle for every loop iteration to avoid caching
+		f, err := os.Open("/proc")
+		if err != nil {
+			b.Error(err)
+		}
+
+		_, err = f.Readdir(0)
 		if err != nil {
 			b.Error(err)
 		}
